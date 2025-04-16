c48a42cbb26763e5bd0a536e3dcee478064efe7b
Kirk Davies <davi0534@algonquinlive.com>
2025-04-16 07:31:59 -0400

    Saving latest changes, updated website, removed phone number

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,4 +1,4 @@
 export const APP_NAME = 'The Mantis™ Mirror System';
 export const PHONE_NUMBER = '+1 (613) 830-1300';
 
-export const EMAIL = 'mkelly@wemeanfitness.com';
+export const EMAIL = 'support@mantismirror.com';
