7c98c874751295d8e5e583d3467f97661dbc0e6f
mkellypts <127528021+mkellypts@users.noreply.github.com>
2025-11-16 11:04:41 -0500

    Add helmet fit link to FAQ

--- src/app/components/faq/faq.component.ts
+++ src/app/components/faq/faq.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NgIf } from "@angular/common";
+import { MatDialog, MatDialogModule } from "@angular/material/dialog";
+import { HelmetFitDialogComponent } from "../helmet-fit-dialog/helmet-fit-dialog.component";
 
 @Component({
   selector: 'app-faq',
   standalone: true,
   imports: [
-    NgIf
+    NgIf,
+    MatDialogModule
   ],
   templateUrl: './faq.component.html',
   styleUrl: './faq.component.scss'
 })
 export class FaqComponent {
   imageVisible = false;
+
+  private readonly dialog = inject(MatDialog);
+
+  openHelmetFitDialog(event: Event): void {
+    event.preventDefault();
+    this.dialog.open(HelmetFitDialogComponent, {
+      maxWidth: '640px',
+      width: '100%',
+      panelClass: 'helmet-fit-dialog'
+    });
+  }
 }
